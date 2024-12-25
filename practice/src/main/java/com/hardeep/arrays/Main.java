c0ac06243efd905be54459f4882965f6880fcb01
harde <harde@192.168.1.2>
2024-12-25 11:20:59 +0530

    Main method test

---
+++ practice/src/main/java/com/hardeep/arrays/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.hardeep.arrays;
+
+/**
+ * @author hardeep
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hardeep");
+
+	}
+
+}
