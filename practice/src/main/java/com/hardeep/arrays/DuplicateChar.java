0465f1f3e036e0e867b3876015fea69764777420
Hardeep Singh <hardeep4.singh@airtel.com>
2024-12-26 20:04:33 +0530

    duplicate character without ascii

--- practice/src/main/java/com/hardeep/arrays/DuplicateChar.java
+++ practice/src/main/java/com/hardeep/arrays/DuplicateChar.java
@@ -3,12 +3,16 @@
  */
 package com.hardeep.arrays;
 
+import java.util.Arrays;
+
 /**
  * @author harde
  *
  */
 public class DuplicateChar {
 
+	//Time complexity = O(n)
+	//Space complexity = O(1)
 	public static boolean isDuplicateStringCaseSensitiveASCII(String str) {
 		if (str ==null || str.length() > 128)
 			return true;
@@ -27,6 +31,8 @@ public class DuplicateChar {
 		return false;
 	}
 	
+	//Time complexity = O(n)
+	//Space complexity = O(1)
 	public static boolean isDuplicateStringCaseInsensitiveASCII(String str) {
 		if (str ==null || str.length() > 128)
 			return true;
@@ -41,6 +47,18 @@ public class DuplicateChar {
 				return true;
 
 			char_arr[char_val] = true;
+		}
+		return false;
+	}
+	//Time complexity = O(n)
+	//Space complexisty = O(1) no extra DS declared so no space
+	public static boolean isDuplicateWithoutASCII(String str) {
+		char[] str_arr = str.toCharArray();
+		Arrays.sort(str_arr);
+		for(int i=0;i<str_arr.length-1;i++) {
+			if(str_arr[i]==str_arr[i+1]) {
+				return true;
+			}
 		}
 		return false;
 	}
--- practice/src/main/java/com/hardeep/arrays/Main.java
+++ practice/src/main/java/com/hardeep/arrays/Main.java
@@ -13,11 +13,10 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String str = "Hhardep";
-		System.out.println(" is duplicate : "+DuplicateChar.isDuplicateStringCaseSensitiveASCII(str));
-		System.out.println(" is duplicate : "+DuplicateChar.isDuplicateStringCaseInsensitiveASCII(str));
-
-                System.out.println("test");
+		String str = "Hhardep ";
+		System.out.println(" is duplicate isDuplicateStringCaseSensitiveASCII : "+DuplicateChar.isDuplicateStringCaseSensitiveASCII(str));
+		System.out.println(" is duplicate isDuplicateStringCaseInsensitiveASCII: "+DuplicateChar.isDuplicateStringCaseInsensitiveASCII(str));
+		System.out.println(" is duplicate isDuplicateWithoutASCII: "+DuplicateChar.isDuplicateWithoutASCII(str));
 	}
 
 }
